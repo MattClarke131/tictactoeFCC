03a95d3e886f74df5d4ffaafa58a20b63e4ba891
Matthew Clarke <mattclarkeruns@gmail.com>
2017-11-07 14:09:46 -0500

    create updateHTML function

--- controller.js
+++ controller.js
@@ -3,8 +3,18 @@ console.log("controller.js loaded");
 
 Tictac.Controller = function() {
   return {
+    ticTacBox: document.getElementsByClassName("ticTacToeBox")[0],
     model: Tictac.Model(),
     initialize: function() {
+    updateHTML: function() {
+      var controller = this;
+      var buttons = controller.ticTacBox.getElementsByClassName("tictactoeButton");
+      for (i=0;i<9;i++) {
+        var xcoord = buttons[i].getAttribute("data-xaxis");
+        var ycoord = buttons[i].getAttribute("data-yaxis");
+        buttons[i].getElementsByTagName('p')[0].innerHTML =
+          controller.model.getSquare(xcoord,ycoord);
+      }
     },
   };
 };
